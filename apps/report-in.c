535280384787a9b32ad4bee7dca2ff5d2e5a6d56
kkeys <kkeys>
2013-09-06 19:53:32 +0000

    uses dbats_bulk_get_key_id()

--- apps/report-in.c
+++ apps/report-in.c
@@ -24,6 +24,7 @@ struct entry {
     dbats_value value;
 };
 char *keys[10000000];
+uint32_t keyids[10000000];
 struct entry data[10000000];
 
 static void write_data(dbats_handler *handler, int select_flags, uint32_t t,
@@ -134,42 +135,59 @@ int main(int argc, char *argv[]) {
 	if (dbats_aggregate(handler, DBATS_AGG_SUM, 10) != 0)
 	    return -1;
     }
-    if (!init_only) dbats_commit(handler); // commit the txn started by dbats_open
 
-    uint32_t key_id = 0;
-    while (1) {
-	int n = scanf("%127s %" SCNval " %" SCNu32 "\n", key, &value, &t);
-	if (n != 3) break;
+    if (init_only) {
+	uint32_t n_keys = 0;
+	while (1) {
+	    int n = scanf("%127s %" SCNval " %" SCNu32 "\n", key, &value, &t);
+	    if (n != 3) break;
 
-	if (t != last_t) {
-	    if (n_data > 0) {
-		write_data(handler, select_flags, last_t, data, n_data);
-		n_data = 0;
+	    if (n_keys > 0 && t != last_t)
+		break;
+	    last_t = t;
+
+	    keys[n_keys++] = strdup(key);
+	}
+
+	if (n_keys > 0) {
+	    dbats_bulk_get_key_id(handler, n_keys, (const char * const *)keys, keyids, DBATS_CREATE);
+	}
+
+    } else {
+	dbats_commit(handler); // commit the txn started by dbats_open
+	uint32_t key_id = 0;
+	while (1) {
+	    int n = scanf("%127s %" SCNval " %" SCNu32 "\n", key, &value, &t);
+	    if (n != 3) break;
+
+	    if (t != last_t) {
+		if (n_data > 0) {
+		    write_data(handler, select_flags, last_t, data, n_data);
+		    n_data = 0;
+		}
+		key_id = 0;
 	    }
-	    key_id = 0;
-	}
-	last_t = t;
+	    last_t = t;
 
-	if (!keys[key_id] || strcmp(keys[key_id], key) != 0) {
-	    // If input keys are in consistent order, this lookup will be
-	    // skipped after the first round.
-	    if (dbats_get_key_id(handler, key, &key_id, DBATS_CREATE) != 0)
-		return -1;
-	    if (keys[key_id]) free(keys[key_id]); // shouldn't happen
-	    keys[key_id] = strdup(key);
-	}
+	    if (!keys[key_id] || strcmp(keys[key_id], key) != 0) {
+		// If input keys are in consistent order, this lookup will be
+		// skipped after the first round.
+		if (dbats_get_key_id(handler, key, &key_id, DBATS_CREATE) != 0)
+		    return -1;
+		if (keys[key_id]) free(keys[key_id]); // shouldn't happen
+		keys[key_id] = strdup(key);
+	    }
 
-	if (!init_only) {
 	    data[n_data].key_id = key_id;
 	    data[n_data].value = value;
 	    n_data++;
+	    key_id++;
 	}
-	key_id++;
-    }
 
-    if (n_data > 0) {
-	write_data(handler, select_flags, last_t, data, n_data);
-	n_data = 0;
+	if (n_data > 0) {
+	    write_data(handler, select_flags, last_t, data, n_data);
+	    n_data = 0;
+	}
     }
 
     elapsed = time(NULL) - run_start;
