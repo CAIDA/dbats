b9e2733d680c31c5185b9ffd9ad0fa9d3b768457
kkeys <kkeys>
2013-10-03 19:58:38 +0000

    key lookups are done in context of a snapshot's transaction

--- apps/report-in.c
+++ apps/report-in.c
@@ -20,16 +20,13 @@ static void help(void) {
     exit(-1);
 }
 
-struct entry {
-    uint32_t key_id;
-    dbats_value value;
-};
 char *keys[10000000];
 uint32_t keyids[10000000];
-struct entry data[10000000];
+dbats_value data[10000000];
+uint32_t n_keys = 0;
+uint32_t n_unknown_keys = 0;
 
-static int write_data(dbats_handler *handler, int select_flags, uint32_t t,
-    struct entry *dat, int n_data)
+static int write_data(dbats_handler *handler, int select_flags, uint32_t t)
 {
     int rc;
     dbats_snapshot *snapshot;
@@ -41,15 +38,22 @@ retry:
 	dbats_log(LOG_ERROR, "error in dbats_select_time()");
 	return -1;
     }
-    for (int i = 0; i < n_data; i++) {
-	rc = dbats_set(snapshot, dat[i].key_id, &dat[i].value);
+
+    if (n_unknown_keys > 0) {
+	dbats_log(LOG_INFO, "dbats_bulk_get_key_id: %d unknown keys", n_unknown_keys);
+	rc = dbats_bulk_get_key_id(handler, snapshot, n_keys, (const char * const *)keys, keyids, DBATS_CREATE);
+	n_unknown_keys = 0;
+    }
+
+    for (int i = 0; i < n_keys; i++) {
+	rc = dbats_set(snapshot, keyids[i], &data[i]);
 	if (rc != 0) {
 	    dbats_abort_snap(snapshot);
 	    if (rc == DB_LOCK_DEADLOCK) {
 		dbats_log(LOG_WARNING, "deadlock in dbats_set()");
 		goto retry;
 	    }
-	    dbats_log(LOG_ERROR, "error in dbats_set()");
+	    dbats_log(LOG_ERROR, "error in dbats_set(): %s", db_strerror(rc));
 	    return -1;
 	}
     }
@@ -60,9 +64,10 @@ retry:
 	    dbats_log(LOG_WARNING, "deadlock in dbats_commit()");
 	    goto retry;
 	}
-	dbats_log(LOG_ERROR, "error in dbats_commit()");
+	dbats_log(LOG_ERROR, "error in dbats_commit(): %s", db_strerror(rc));
 	return -1;
     }
+    n_keys = 0;
     return 0;
 }
 
@@ -81,7 +86,6 @@ int main(int argc, char *argv[]) {
 
     uint32_t last_t = 0;
     uint32_t t;
-    int n_data = 0;
     dbats_value value;
     char key[128];
 
@@ -145,7 +149,6 @@ int main(int argc, char *argv[]) {
     }
 
     if (init_only) {
-	uint32_t n_keys = 0;
 	while (1) {
 	    int n = scanf("%127s %" SCNval " %" SCNu32 "\n", key, &value, &t);
 	    if (n != 3) break;
@@ -164,39 +167,33 @@ int main(int argc, char *argv[]) {
 
     } else {
 	dbats_commit_open(handler); // commit the txn started by dbats_open
-	uint32_t key_id = 0;
 	while (1) {
 	    int n = scanf("%127s %" SCNval " %" SCNu32 "\n", key, &value, &t);
 	    if (n != 3) break;
 
 	    if (t != last_t) {
-		if (n_data > 0) {
-		    rc = write_data(handler, select_flags, last_t, data, n_data);
-		    n_data = 0;
+		if (n_keys > 0) {
+		    rc = write_data(handler, select_flags, last_t);
 		    if (rc != 0) break;
 		}
-		key_id = 0;
 	    }
 	    last_t = t;
 
-	    if (!keys[key_id] || strcmp(keys[key_id], key) != 0) {
-		// If input keys are in consistent order, this lookup will be
-		// skipped after the first round.
-		if (dbats_get_key_id(handler, NULL, key, &key_id, DBATS_CREATE) != 0)
-		    return -1;
-		if (keys[key_id]) free(keys[key_id]); // shouldn't happen
-		keys[key_id] = strdup(key);
+	    if (!keys[n_keys] || strcmp(keys[n_keys], key) != 0) {
+		// If input keys are in consistent order, this will only
+		// happen in the first round.
+		keyids[n_keys] = UINT32_MAX;
+		n_unknown_keys++;
+		if (keys[n_keys]) free(keys[n_keys]); // shouldn't happen
+		keys[n_keys] = strdup(key);
 	    }
 
-	    data[n_data].key_id = key_id;
-	    data[n_data].value = value;
-	    n_data++;
-	    key_id++;
+	    data[n_keys] = value;
+	    n_keys++;
 	}
 
-	if (n_data > 0) {
-	    rc = write_data(handler, select_flags, last_t, data, n_data);
-	    n_data = 0;
+	if (n_keys > 0) {
+	    rc = write_data(handler, select_flags, last_t);
 	}
     }
 
