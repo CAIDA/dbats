595e899cb5d3871883784642386aafe6dfaacf69
kkeys <kkeys>
2013-10-12 01:34:26 +0000

    stores open dbats_handle in procstate.dht     - stores a separate handle for each dbatsPath     - reuses open handle instead of opening a new one for every http request     - allows multiple simultaneous requests to the same or different dbats dbs     - closes all open handles when process exits debugging logging

--- lib/mod_dbats.c
+++ lib/mod_dbats.c
@@ -8,29 +8,46 @@
 #include "http_log.h"
 #include "ap_config.h"
 #include "apr_strings.h"
+#include "apr_hash.h"
 #include "dbats.h"
 
 typedef struct {
+    const char *context;
     const char *path;
-    dbats_handler *dh;
-} mod_dbats_config;
+    int n;
+} mod_dbats_dir_config;
+
+struct {
+    apr_pool_t *pool;
+    apr_thread_mutex_t *mutex;
+    apr_hash_t *dht; // hash table of dbats_path -> dbats_handler
+} procstate;
 
 module AP_MODULE_DECLARE_DATA dbats_module;
 
 static const char *set_path(cmd_parms *cmd, void *vcfg, const char *arg)
 {
-    mod_dbats_config *cfg = (mod_dbats_config*)vcfg;
+    mod_dbats_dir_config *cfg = (mod_dbats_dir_config*)vcfg;
     cfg->path = arg;
     return NULL;
 }
 
+static apr_status_t mod_dbats_close(void *data)
+{
+    dbats_handler *dh = (dbats_handler*)data;
+    ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, procstate.pool, "mod_dbats: mod_dbats_close pid=%u:%lu", getpid(), pthread_self());
+    int rc = dbats_close(dh);
+    ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, procstate.pool, "mod_dbats: dbats_close: %d", rc);
+    return APR_SUCCESS;
+}
+
 static void child_init_handler(apr_pool_t *pchild, server_rec *s)
 {
-    //mod_dbats_config *cfg = (mod_dbats_config*)ap_get_module_config(r->per_dir_config, &dbats_module);
-    ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, pchild, "mod_dbats: child_init_handler pid=%u", getpid());
-    //dbats_log_level = 99;
-    //int rc = dbats_open(&cfg->dh, cfg->path, 1, 60, DBATS_READONLY);
-    //ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open returned %d: %s", rc, db_strerror(rc));
+    ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, pchild, "############ mod_dbats: child_init_handler pchild=%08x pid=%u:%lu", (unsigned)pchild, getpid(), pthread_self());
+    dbats_log_level = DBATS_LOG_FINE;
+    procstate.dht = apr_hash_make(pchild);
+    procstate.pool = pchild;
+    apr_thread_mutex_create(&procstate.mutex, APR_THREAD_MUTEX_DEFAULT, pchild);
 }
 
 // Parse URL query parameters into a table
@@ -61,20 +78,30 @@ static void args_to_table(request_rec *r, apr_table_t **tablep)
 static int metrics_find(request_rec *r)
 {
     int result = OK;
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: req_handler pid=%u", getpid());
-    //dbats_log_level = 99;
+    int rc;
 
-    mod_dbats_config *cfg = (mod_dbats_config*)ap_get_module_config(r->per_dir_config, &dbats_module);
+    mod_dbats_dir_config *cfg = (mod_dbats_dir_config*)ap_get_module_config(r->per_dir_config, &dbats_module);
+    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "dir_cfg: %08x, n=%d, context=%s, path=%s", (unsigned)cfg, ++cfg->n, cfg->context, cfg->path);
 
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open %s", cfg->path);
-    int rc = dbats_open(&cfg->dh, cfg->path, 1, 60, DBATS_READONLY);
-    if (rc != 0) {
-	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open: %s", db_strerror(rc));
-	return HTTP_INTERNAL_SERVER_ERROR;
+    apr_thread_mutex_lock(procstate.mutex);
+    dbats_handler *dh = (dbats_handler*)apr_hash_get(procstate.dht, cfg->path, APR_HASH_KEY_STRING);
+    if (dh) {
+	apr_thread_mutex_unlock(procstate.mutex);
+    } else {
+	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "######## mod_dbats: dbats_open %s", cfg->path);
+	rc = dbats_open(&dh, cfg->path, 1, 60, DBATS_READONLY);
+	if (rc != 0) {
+	    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open: %s", db_strerror(rc));
+	    apr_thread_mutex_unlock(procstate.mutex);
+	    return HTTP_INTERNAL_SERVER_ERROR;
+	}
+	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open: ok");
+	apr_hash_set(procstate.dht, cfg->path, APR_HASH_KEY_STRING, dh);
+	apr_pool_cleanup_register(procstate.pool, dh, mod_dbats_close, NULL);
+	apr_thread_mutex_unlock(procstate.mutex);
+	dbats_commit_open(dh);
+	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_commit_open: ok");
     }
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_open: ok");
-    dbats_commit_open(cfg->dh);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_commit_open: ok");
 
     apr_table_t *GET;
     args_to_table(r, &GET);
@@ -82,17 +109,13 @@ static int metrics_find(request_rec *r)
     const char *format = apr_table_get(GET, "format");
     const char *logLevel = apr_table_get(GET, "logLevel");
 
-#if 1
     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# query: %s", query);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# format: %s", format);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# logLevel: %s", logLevel);
-#endif
 
     dbats_keytree_iterator *dki;
     uint32_t keyid;
     char name[DBATS_KEYLEN];
 
-    rc = dbats_glob_keyname_start(cfg->dh, NULL, &dki, query);
+    rc = dbats_glob_keyname_start(dh, NULL, &dki, query);
     if (rc != 0) {
 	ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "mod_dbats: dbats_glob_keyname_start: %s", db_strerror(rc));
 	return HTTP_INTERNAL_SERVER_ERROR;
@@ -142,7 +165,6 @@ static int metrics_find(request_rec *r)
     }
 
     dbats_glob_keyname_end(dki);
-    dbats_close(cfg->dh);
 
     return result;
 }
@@ -160,12 +182,8 @@ static int req_handler(request_rec *r)
     if (!r->handler || strcmp(r->handler, "dbats-handler") != 0)
 	return DECLINED;
 
+    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "######## mod_dbats: req_handler pid=%u:%lu", getpid(), pthread_self());
     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# the_request: %s", r->the_request);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# hostname: %s", r->hostname);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# status_line: %s", r->status_line);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# method: %s", r->method);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# content_encoding: %s", r->content_encoding);
-    ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# unparsed_uri: %s", r->unparsed_uri);
     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# uri: %s", r->uri);
     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# filename: %s", r->filename);
     ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "# canonical_filename: %s", r->canonical_filename);
@@ -174,22 +192,36 @@ static int req_handler(request_rec *r)
 
     if (ends_with(r->uri, "/metrics/find/"))
 	return metrics_find(r);
-    return HTTP_NOT_FOUND;
+    else
+	return HTTP_NOT_FOUND;
 }
 
 static void *create_dir_conf(apr_pool_t *pool, char *context)
 {
-    if (!context) context = "(undefined context)";
-    mod_dbats_config *cfg = apr_pcalloc(pool, sizeof(mod_dbats_config));
+    ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, pool, "create_dir_conf: pid=%u:%lu, context=%s", getpid(), pthread_self(), context);
+    mod_dbats_dir_config *cfg = apr_pcalloc(pool, sizeof(mod_dbats_dir_config));
     if (cfg) {
 	cfg->path = NULL;
+	cfg->context = apr_pstrdup(pool, context);
+	cfg->n = 0;
     }
     return cfg;
 }
 
+static void *merge_dir_conf(apr_pool_t *pool, void *vbase, void *vadd)
+{
+    mod_dbats_dir_config *base = (mod_dbats_dir_config *)vbase;
+    mod_dbats_dir_config *add = (mod_dbats_dir_config *)vadd;
+
+    char *context = apr_psprintf(pool, "(%s) + (%s)", base->context, add->context);
+    mod_dbats_dir_config *merged = (mod_dbats_dir_config *)create_dir_conf(pool, context);
+    merged->path = add->path ? add->path : base->path;
+
+    return merged;
+}
+
 static void register_hooks(apr_pool_t *pool)
 {
-#if 1
     int major, minor;
     db_version(&major, &minor, NULL);
     if (major != DB_VERSION_MAJOR || minor != DB_VERSION_MINOR) {
@@ -198,10 +230,8 @@ static void register_hooks(apr_pool_t *pool)
 	    major, minor, DB_VERSION_MAJOR, DB_VERSION_MINOR);
 	return;
     }
-#endif
 
     ap_log_perror(APLOG_MARK, APLOG_NOTICE, 0, pool, "mod_dbats: register_hooks");
-    //dbats_log_level = 99;
     ap_hook_handler(req_handler, NULL, NULL, APR_HOOK_MIDDLE);
     ap_hook_child_init(child_init_handler, NULL, NULL, APR_HOOK_MIDDLE);
 }
@@ -216,7 +246,7 @@ module AP_MODULE_DECLARE_DATA dbats_module =
 {
     STANDARD20_MODULE_STUFF,
     create_dir_conf,      /* Per-directory configuration handler */
-    NULL,                 /* Merge handler for per-directory configurations */
+    merge_dir_conf,       /* Merge handler for per-directory configurations */
     NULL,                 /* Per-server configuration handler */
     NULL,                 /* Merge handler for per-server configurations */
     directives,           /* Any directives we may have for httpd */
